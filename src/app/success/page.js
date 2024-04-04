52270cf8bb0f1be8e26d6e33c63ccf6b415e06f2
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-04-04 16:32:20 -0300

    @

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from "react";
 
 const Page = () => {
   const searchParams = useSearchParams();
-
+//?orderID=660effc12c2b1665f2fec3a0&collection_id=75600689512&collection_status=approved&payment_id=75600689512&status=approved&external_reference=null&payment_type=account_money&merchant_order_id=17306289685&preference_id=743465031-d887ba61-b9f3-45fc-8c6e-98d30a7645bb&site_id=MLA&processing_mode=aggregator&merchant_account_id=null
   const getOrder = async (_id) => {
     const response = await axios.get("/api/orders");
     console.log(response.data.filter((e) => e._id == _id));
@@ -22,7 +22,7 @@ const Page = () => {
 
   useEffect(() => {
     const mpPaymentId = searchParams.get("payment_id");
-    const mpStatus = searchParams.get("status");
+    const mpStatus = searchParams.get("collection_status");
     const orderId = searchParams.get("orderID");
   
     if (orderId) {
