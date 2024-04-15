fd1df64f5acb0b6b4116f7c5a38cfe206e0fbdcf
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-04-15 14:32:28 -0300

    @

--- src/app/perfil/[username]/page.js
+++ src/app/perfil/[username]/page.js
@@ -58,7 +58,7 @@ const Page = () => {
                 />
               </div>
               <div className="flex justify-center mt-5 ">
-                <Card className="w-10/12 bg-white/40">
+                <Card className="w-12/12 md:w-10/12 bg-white/40">
                   <CardHeader className="">
                     <div className="w-full  flex">
                       <p className="capitalize font-geist font-bold tracking-tighter ">
@@ -77,28 +77,30 @@ const Page = () => {
                       ))}
                     </div>
                   </CardHeader>
-                  <CardContent className="flex items-start font-semibold tracking-tighter font-geist">
-                    <svg
-                      width={60}
-                      className="mr-2"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 512 512"
-                    >
-                      <g>
-                        <g
-                          fill="none"
-                          fillRule="evenodd"
-                          stroke="none"
-                          strokeWidth="1"
-                        >
-                          <g fill="#000" transform="translate(42.667 42.667)">
-                            <path d="M213.333 0c117.822 0 213.334 95.512 213.334 213.333 0 117.82-95.512 213.334-213.334 213.334C95.513 426.667 0 331.154 0 213.333S95.513 0 213.333 0zm21.38 192h-42.666v128h42.666V192zm-21.163-90.667c-15.554 0-26.837 11.22-26.837 26.371 0 15.764 10.986 26.963 26.837 26.963 15.235 0 26.497-11.2 26.497-26.667 0-15.446-11.262-26.667-26.497-26.667z"></path>
+                  {user.bio && (
+                    <CardContent className="flex items-start font-semibold tracking-tighter font-geist">
+                      <svg
+
+                        className="mr-2 w-[60px]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 512 512"
+                      >
+                        <g>
+                          <g
+                            fill="none"
+                            fillRule="evenodd"
+                            stroke="none"
+                            strokeWidth="1"
+                          >
+                            <g fill="#000" transform="translate(42.667 42.667)">
+                              <path d="M213.333 0c117.822 0 213.334 95.512 213.334 213.333 0 117.82-95.512 213.334-213.334 213.334C95.513 426.667 0 331.154 0 213.333S95.513 0 213.333 0zm21.38 192h-42.666v128h42.666V192zm-21.163-90.667c-15.554 0-26.837 11.22-26.837 26.371 0 15.764 10.986 26.963 26.837 26.963 15.235 0 26.497-11.2 26.497-26.667 0-15.446-11.262-26.667-26.497-26.667z"></path>
+                            </g>
                           </g>
                         </g>
-                      </g>
-                    </svg>
-                    {user.bio}
-                  </CardContent>
+                      </svg>
+                      {user.bio}
+                    </CardContent>
+                  )}
                 </Card>
               </div>
               <div className="w-full flex justify-center mt-5">
@@ -106,33 +108,10 @@ const Page = () => {
               </div>
             </div>
             <div className=" ">
-              <div className="flex-1 flex justify-start   flex-col">
+              <div className="flex-1 flex justify-start   flex-col pb-10">
                 <div className="flex justyfy-center ">
                   <div className="w-full ">
-                    <div className="flex justify-center">
-                      {user.videos.length > 0 && (
-                        <Carousel className="w-9/12 md:w-10/12 mt-5 ">
-                          <CarouselContent className="">
-                            {user.videos.map((e) => (
-                              <CarouselItem
-                                key={e}
-                                className=" flex justify-center items-center"
-                              >
-                                <ReactPlayer
-                                  url={e}
-                                  controls={true}
-                                  style={{
-                                    maxWidth: "80vw",
-                                  }}
-                                />
-                              </CarouselItem>
-                            ))}
-                          </CarouselContent>
-                          <CarouselPrevious />
-                          <CarouselNext />
-                        </Carousel>
-                      )}
-                    </div>
+             
                     <div className=" mt-2 grid grid-cols-1 md:grid-cols-2   ">
                       {productos &&
                         productos.map((e, index) => (
@@ -161,6 +140,30 @@ const Page = () => {
                             />
                           </div>
                         ))}
+                    </div>
+                    <div className="flex justify-center">
+                      {user.videos.length > 0 && (
+                        <Carousel className="w-9/12 md:w-10/12 mt-5 ">
+                          <CarouselContent className="">
+                            {user.videos.map((e) => (
+                              <CarouselItem
+                                key={e}
+                                className=" flex justify-center items-center"
+                              >
+                                <ReactPlayer
+                                  url={e}
+                                  controls={true}
+                                  style={{
+                                    maxWidth: "80vw",
+                                  }}
+                                />
+                              </CarouselItem>
+                            ))}
+                          </CarouselContent>
+                          <CarouselPrevious />
+                          <CarouselNext />
+                        </Carousel>
+                      )}
                     </div>
                   </div>
                 </div>
