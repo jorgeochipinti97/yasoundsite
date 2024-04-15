a54aa12f4de15b5e301bcab4bb6665499cbc9c4c
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-04-15 20:05:14 -0300

    2'

--- pages/api/update.js
+++ pages/api/update.js
@@ -1,23 +1,55 @@
-import { connectDB } from '@/lib/database';
-import User from '@/models/User';
+import sgMail from "@sendgrid/mail";
+import { connectDB } from "@/lib/database";
+import Waitlist from "@/models/Waitlist";
+import User from "@/models/User";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default async function handler(req, res) {
-    if (req.method !== 'POST') {
-        // Restringe el método a POST para evitar cambios no deseados
-        return res.status(405).json({ message: 'Method not allowed' });
-    }
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
 
-    try {
-        // Conectar a la base de datos
-        await connectDB();
+  try {
+    await connectDB();
+    const waitlistUsers = await Waitlist.find({});
 
-        // Actualizar todos los usuarios para que sean premium
-        // const updateResult = await User.updateMany({}, { $set: { premium: true } });
+    const results = await Promise.all(
+      waitlistUsers.map(async (waitlistUser) => {
+        const user = await User.findOne({ email: waitlistUser.email });
+        if (!user) {
+          return {
+            message: `No hay un usuario creado con el email ${waitlistUser.email}.`,
+          };
+        }
 
-        // Responder con el resultado de la actualización
-        res.status(200).json({ message: 'Todos los usuarios han sido actualizados a premium.', data: updateResult });
-    } catch (error) {
-        console.error('Error updating users to premium:', error);
-        res.status(500).json({ message: 'Failed to update users to premium', error: error.message });
-    }
+        const resetPasswordUrl = `https://www.yasound.site/regenerate?id=${user._id}`;
+        const imageUrl = "https://www.yasound.site/images/banner.jpeg"; // Reemplaza esto con la URL real de tu imagen
+        const msg = {
+          to: user.email,
+          from: "contacto@yasound.site",
+          subject: "Recuperación de Contraseña",
+          text: "Aquí puedes escribir instrucciones para establecer la contraseña",
+          html: ` <img src="${imageUrl}" alt="Banner" style="width:100%; max-width:600px;">
+                <p><strong><a href="${resetPasswordUrl}" target="_blank">Aquí puedes cambiar tu contraseña</a></strong></p>
+            `,
+        };
+
+        await sgMail.send(msg);
+        return { message: `Correo de recuperación enviado a ${user.email}` };
+      })
+    );
+
+    res
+      .status(200)
+      .json({ message: "Emails de recuperación enviados", data: results });
+  } catch (error) {
+    console.error("Error al enviar correos de recuperación:", error);
+    res
+      .status(500)
+      .json({
+        message: "Error al procesar la solicitud",
+        error: error.message,
+      });
+  }
 }
