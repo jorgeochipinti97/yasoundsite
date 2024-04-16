cc92deee72fb5c32394e50bec94cc9d2b68b6062
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-04-16 10:42:43 -0300

    @

--- src/app/billing/page.js
+++ src/app/billing/page.js
@@ -14,6 +14,17 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
   const { user } = useUsers();
   const [transactions, setTransactiosn] = useState([]);
@@ -25,28 +36,28 @@ const Page = () => {
 
     setTransactiosn(
       data.data.filter(
-        (e) => e.seller == user.username && e.status == "approved" ||e.seller == user._id && e.status == "approved" 
+        (e) =>
+          (e.seller == user.username && e.status == "approved") ||
+          (e.seller == user._id && e.status == "approved")
       )
     );
 
-
     const totalUSD = transactions.reduce((total, transaction) => {
-        if (transaction.provider === 'paypal') {
-          return total + transaction.amount;
-        }
-        return total;
-      }, 0);
-      
-      const totalARS = transactions.reduce((total, transaction) => {
-        if (transaction.provider === 'mercadopago') {
-          return total + transaction.amount;
-        }
-        return total;
-      }, 0);
+      if (transaction.provider === "paypal") {
+        return total + transaction.amount;
+      }
+      return total;
+    }, 0);
 
-      setTotalArs(totalARS)
-      setTotalUsd(totalUSD)
+    const totalARS = transactions.reduce((total, transaction) => {
+      if (transaction.provider === "mercadopago") {
+        return total + transaction.amount;
+      }
+      return total;
+    }, 0);
 
+    setTotalArs(totalARS);
+    setTotalUsd(totalUSD);
   };
   useEffect(() => {
     user && getOrders();
@@ -67,25 +78,42 @@ const Page = () => {
         </div>
       </section>
 
+      <div className="flex justify-center w-full mt-5">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Como retirar saldo PayPal</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogDescription>
+                El monto mínimo para retiros es de $10. Tenga en cuenta que
+                PayPal aplica comisiones adicionales. Para realizar cualquier
+                retiro, por favor contáctenos.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
       <section className="flex justify-center">
         <div className="bg-white w-11/12 md:w-6/12 border-2 mt-10 rounded-xl">
           <Table className="">
-         
             <TableHeader>
               <TableRow>
                 <TableHead className="text-xs">Comprador</TableHead>
-                <TableHead className='text-xs'>Producto</TableHead>
-                <TableHead className='text-xs'>Precio</TableHead>
+                <TableHead className="text-xs">Producto</TableHead>
+                <TableHead className="text-xs">Precio</TableHead>
                 <TableHead className="text-xs"></TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {transactions.map((e,index) => (
+              {transactions.map((e, index) => (
                 <TableRow key={index}>
                   <TableCell className="text-xs">{e.buyer}</TableCell>
-                  <TableCell className='text-xs'>{e.product || ""}</TableCell>
-                  <TableCell className='text-xs'>${e.amount}</TableCell>
-                  <TableCell className="text-xs">{e.provider || "mercadopago"}</TableCell>
+                  <TableCell className="text-xs">{e.product || ""}</TableCell>
+                  <TableCell className="text-xs">${e.amount}</TableCell>
+                  <TableCell className="text-xs">
+                    {e.provider || "mercadopago"}
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
