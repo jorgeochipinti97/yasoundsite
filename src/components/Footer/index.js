68758aca88b80ace4f73b19f65bbcb5f5059e2a7
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2024-04-16 14:30:01 -0300

    @

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -76,6 +76,18 @@ export const FooterComponent = () => {
         </a>
       </div>
       <Button onClick={() => push("https://wa.link/vsobjt")} className='mt-5'>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+      className="mr-2"
+      width={25}
+      >
+      <path
+        stroke="#f5f5f7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M8 11h.01M12 11h.01M16 11h.01M21 20l-3.324-1.662a4.161 4.161 0 00-.51-.234 2.007 2.007 0 00-.36-.085c-.139-.019-.28-.019-.561-.019H6.2c-1.12 0-1.68 0-2.108-.218a2 2 0 01-.874-.874C3 16.48 3 15.92 3 14.8V7.2c0-1.12 0-1.68.218-2.108a2 2 0 01.874-.874C4.52 4 5.08 4 6.2 4h11.6c1.12 0 1.68 0 2.108.218a2 2 0 01.874.874C21 5.52 21 6.08 21 7.2V20z"
+      ></path>
+    </svg>
         Obtené ayuda en linea
       </Button>
       <div className="flex mt-5">
